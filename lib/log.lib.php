5e227adfed489d22ed2e6a9b0c7c2947c6dbdef7
George Tsatsis <fafafa609@gmail.com>
2018-01-08 15:43:08 +0200

    Update log.lib.php

--- lib/log.lib.php
+++ lib/log.lib.php
@@ -3,7 +3,8 @@ require('../vendor/autoload.php'); //Composer Autoloader
 require('../../../../pgdbcreds.inc.php'); // Database Credentials
 
 function logtoDB($token,$fileName,$originalFileName,$timestampOfUpload,$md5Hash,$SHA1Hash){
-	$logQuery = "INSERT INTO logs (token, filename, originalfilename, timestamp, md5hash, sha1hash) VALUES ('$token', '$filename', '$originalFileName', '$timestampOfUpload', '$md5Hash', '$SHA1Hash')";
+	$logQuery = "INSERT INTO logs (token, filename, originalfilename, timestamp, md5hash, sha1hash) VALUES ('$token', '$fileName', '$originalFileName', '$timestampOfUpload', '$md5Hash', '$SHA1Hash')";
 	$logResult = pg_exec($database, $logQuery);
 }
 ?>
+
