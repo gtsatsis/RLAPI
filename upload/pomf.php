19ac242867fa6fe0b8def4e441d4429241a83d17
MucaP <mucapapipa.br@gmail.com>
2018-01-08 12:49:18 -0200

    Pomf PSR-1 file

--- upload/pomf.php
+++ upload/pomf.php
@@ -1,87 +1,153 @@
 <?php
 
+/**
+ * PHP version 5
+ *
+ * @category  File_Uploading
+ * @package   RLME\RLAPI
+ * @author    Samuel Simão <samuel@pomaire.com.br>
+ * @author    George Tsatsis <admin@ratelimited.me>
+ * @copyright 2017-2018 RATELIMITED, LLC
+ * @license   MIT <https://opensource.org/licenses/MIT>
+ * @version   GIT: <git_id>
+ * @link      https://ratelimited.me
+ * @see       S3Client
+ * @since     File available since RLAPI 2.0
+ */
 
+ /**
+  * Set PHP to report errors (any nature)
+  *
+  * @todo Remove when in production to avoid attacks and exploits
+  */
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-// This project requires:
-require('../vendor/autoload.php'); // Composer Autoloader
-require('../lib/authorize2.php'); // Authorization Library
-require('../lib/log-lib.php'); // Logging Library
-require('../speechlines.inc.php'); // Speech Lines ($dbConnFailed etc)
-require('../../../../S3APICredStore/s3Credentials.inc.php'); // S3 API Creds
-require('../../../../pgdbcreds.inc.php'); // Database Credentials
-// Set variables
+/* This project requires the following files: */
+require '../vendor/autoload.php'; // Composer Auto-loader
+require '../lib/authorize2.php'; // Authorization Library
+require '../speechlines.inc.php'; // Speech Lines ($dbConnFailed etc)
+require '../../../../S3APICredStore/s3Credentials.inc.php'; // S3 API Credentials
+require '../../../../pgdbcreds.inc.php'; // Database Credentials
 
-
+// Pass all the GET parameters to an array
 parse_str($_SERVER['QUERY_STRING'], $get_array);
+
+// Extract the token (?key) to the $token variable
 $token = $get_array['key'];
 
-if(is_null($token) or !isset($token)) {
-	echo $noToken;
-	return;
+/* If no token is provided, report to the user and die */
+if (is_null($token) or !isset($token)) {
+    echo $noToken;
+    return;
 }
 
+// Initialize AWS credentials with values from the previously required file
 $credentials = new Aws\Credentials\Credentials($s3APIKey, $s3APISecret);
 
-// Code Starts Here
+// Authenticate using the provided token
 authenticate($token);
 
-if($allowed == "true"){
-	$s3 = new Aws\S3\S3Client([
-    'version' => 'latest',
-    'region'  => 'us-east-1',
-    'endpoint' => 'http://127.0.0.1:9000/',
-    'credentials' => $credentials,
-    //'signature'  => 'v4',
-    'use_path_style_endpoint' => true // Minio Compatibility (https://minio.io)
-]);
+/* If the token is allowed, create s3 client and start proccessing files */
+if ($allowed == "true") {
+    $s3 = new Aws\S3\S3Client(
+        [
+            'version' => 'latest', // Latest S3 version
+            'region'  => 'us-east-1', // The service's region
+            'endpoint' => 'http://127.0.0.1:9000/', // API to point to
+            'credentials' => $credentials, // Credentials (s3Credentials.inc.php)
+            //'signature'  => 'v4',
+            'use_path_style_endpoint' => true // Minio Compatible (https://minio.io)
+        ]
+    );
 
-foreach ($_FILES['files']['name'] as $files) {
+    /**
+     * Foreach loop to process files
+     * 
+     * @todo Better error handling and reporting
+     */
+    foreach ($_FILES['files']['name'] as $files) {
 
-	/* 
-		|-------------------------------------------------------|
-		|Code taken from StackExchange							|
-		|Permalink: https://stackoverflow.com/a/5439548/8156177 |
-		|-------------------------------------------------------|
-	*/
-	 $extension = pathinfo($files, PATHINFO_EXTENSION); // get file extension
-	 $fileName = substr(str_shuffle(str_repeat("0123456789abcdefghijklmnopqrstuvwxyz", 6)), 0, 6) . "." . $extension; // generate name and concatenate extension
-	 $tmpName = implode('', $_FILES['files']['tmp_name']);
-	 move_uploaded_file($tmpName, "/d2/RLTemp/" . $fileName);
+        /*
+            |-------------------------------------------------------|
+            |Code taken from StackExchange                          |
+            |Permalink: https://stackoverflow.com/a/5439548/8156177 |
+            |-------------------------------------------------------|
+         */
 
-	$fileNames = array(
-	'success' => true,
-	'files' => array(
-	array(
-	 'url' => $fileName,
-	 'name' => implode($_FILES['files']['name']),
-	 'hash_md5' => md5_file('/d2/RLTemp/'.$fileName),
-	 'hash_sha1' => sha1_file('/d2/RLTemp/'.$fileName)
-	)
-       )
-      );
+         // Get the uploaded file's extension
+        $extension = pathinfo($files, PATHINFO_EXTENSION);
 
-	$md5 = md5_file('/d2/RLTemp/'.$fileName);
-	$sha1 = sha1_file('/d2/RLTemp/'.$fileName);
-	
-	 $result = $s3->putObject(array(
-		'Bucket' => 'owoapi',
-		'Key'    => $fileName,
-		'SourceFile'   => "/d2/RLTemp/" . $fileName,
-		'ACL'	=> 'public-read'
-	));
+        // Generate a random name
+        $fileName = substr(
+            str_shuffle(str_repeat("0123456789abcdefghijklmnopqrstuvwxyz", 6)), 0, 6
+        );
 
-	unlink("/d2/RLTemp/" . $fileName);
-	echo json_encode($fileNames);
-	logtoDB($token,$fileName,implode($_FILES['files']['name']),time(),$md5,$sha1);
+        // Add file extension
+        $fileName .= "." . $extension;
 
+        /**
+         * Little hack to convert from array to string by imploding with no 
+         * glue string to avoid "illegal string offset"
+         * 
+         * @todo Replace by something less "hacky"
+         */
+        $tmpName = implode('', $_FILES['files']['tmp_name']);
+
+        /**
+         * Move file to a temporary, unaccessible location.
+         * 
+         * @todo Make the path configurable
+         */
+        move_uploaded_file($tmpName, "/d2/RLTemp/" . $fileName);
+
+        /* Create array with file data */
+        $fileNames = array(
+            'success' => true, // If the user got here, we had success
+            'files' => array( // Add files to an array
+                        array(
+                            // Filename (on the server)
+                            'url' => $fileName,
+                            // The name of the uploaded file (the user's file name)
+                            'name' => implode($_FILES['files']['name']),
+                            /* Hashes (md5 and sha1) */
+                            'hash_md5' => md5_file('/d2/RLTemp/'.$fileName),
+                            'hash_sha1' => sha1_file('/d2/RLTemp/'.$fileName)
+                            )
+                        )
+		);
+		
+		/* Create file hashes (md5 and sha1) */
+		$md5 = md5_file('/d2/RLTemp/'.$fileName);
+		$sha1 = sha1_file('/d2/RLTemp/'.$fileName);
+
+
+        /* Put the file in the Minio/S3 bucket */
+        $result = $s3->putObject(
+            array(
+            'Bucket' => 'owoapi', // Bucket name
+            'Key'    => $fileName, // Key = File name (on the server)
+            'SourceFile'   => "/d2/RLTemp/" . $fileName, // The file to be put
+            'ACL'    => 'public-read' // Access Control List  set to public read
+            )
+        );
+
+        // Delete the file from the temporary location
+        unlink("/d2/RLTemp/" . $fileName);
+
+        // Print the array as JSON for ShareX compatibility
+		echo json_encode($fileNames);
+		
+		// Log to database
+		logtoDB($token,$fileName,implode($_FILES['files']['name']),time(),$md5,$sha1);
+
+    }
+  
+    /* If the  user token is not allowed, throw tokenIsBlocked speech line */
+} elseif ($allowed == "false") {
+    echo $tokenIsBlocked;
+} else {
+    /* If the  user token is not valid, throw invalidToken speech line */
+    echo $invalidToken;
 }
-
-}elseif($allowed == "false"){
-	echo $tokenIsBlocked;
-}else{
-	echo $invalidToken;
-}
-?>
