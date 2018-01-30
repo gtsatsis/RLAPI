710df6d8ebdf296e218bb6d2ad78ce83c229367b
George Tsatsis <fafafa609@gmail.com>
2018-01-30 21:55:21 +0200

    Debug to figure out how the UUID dependency works

--- users/create.php
+++ users/create.php
@@ -1,2 +1,28 @@
 <?php
+/* This project requires the following files: */
+require '../vendor/autoload.php'; // Composer Auto-loader
+require '../lib/authorize2.php'; // Authorization Library
+require '../speechlines.inc.php'; // Speech Lines ($dbConnFailed etc)
+require '../../../rl1-pgdbcreds.inc.php'; // Database Credentials
+/* Composer Usage */
+use Ramsey\Uuid\Uuid;
+use Ramsey\Uuid\Exception\UnsatisfiedDependencyException;
+
+/* Define the token variable*/
+$token = $_GET['key'];
+
+/* If no token is provided, report to the user and die */
+if (is_null($token) or !isset($token)) {
+    echo $noToken;
+    return;
+}
+
+authenticate($token);
+
+if($isAdmin == true){
+  $uuid4 = Uuid::uuid4();
+  print_r($uuid4); //Debug
+  
+}else{
+return "Unauthorized"}
 ?>
