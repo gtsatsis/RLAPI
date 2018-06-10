bfc60a8cd6db93e5443b5cb8a3136b250d1af0d4
George Tsatsis <fafafa609@gmail.com>
2018-06-10 03:43:23 +0300

    SQL Injection Patch -- LogLib (h1 #363840)

--- lib/log-lib.php
+++ lib/log-lib.php
@@ -3,8 +3,8 @@ require('../../../rl1-pgdbcreds.inc.php');
 
 function logtodb($token,$fileName,$originalFileName,$timestampOfUpload,$md5Hash,$SHA1Hash){
 	require('../../../rl1-pgdbcreds.inc.php');
-	$logQuery = "INSERT INTO logs (token, filename, originalfilename, timestamp, md5hash, sha1hash) VALUES ('$token', '$fileName', '$originalFileName', '$timestampOfUpload', '$md5Hash', '$SHA1Hash')";
-	$logResult = pg_exec($database, $logQuery);
+	$logQuery =  pg_prepare($database, "logQuery", "INSERT INTO logs (token, filename, originalfilename, timestamp, md5hash, sha1hash) VALUES ('$1', '$2', '$3', '$4', '$5', '$6')");
+	$logResult = pg_execute($database, "logQuery", array($token, $fileName, $originalFileName, $timestampOfUpload, $md5Hash, $SHA1Hash));
 }
 ?>
 
